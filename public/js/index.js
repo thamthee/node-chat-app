cb4c12e5c4e029e5168bda60d5ddbc0f91b34a82
Tham Treerawuttichaikit <intime_@outlook.com>
2017-11-02 13:25:48 +0700

    Add form for message and show incoming in browser

--- public/js/index.js
+++ public/js/index.js
@@ -4,10 +4,33 @@ socket.on('connect', function () {
   console.log('Connect to server');
 });
 
-socket.on('newMessage', function (newMessage) {
-  console.log('New message from server', newMessage);
+socket.on('newMessage', function (message) {
+  console.log('New message from server', message);
+
+  var li = jQuery('<li></li>');
+  li.text(`${message.from}: ${message.text}`);
+
+  jQuery('#message').append(li);
 });
 
 socket.on('disconnect', function () {
   console.log('Disconnected from server');
+});
+
+// socket.emit('createMessage', {
+//   from: 'John',
+//   text: 'Hola Everybody'
+// }, function (data) {
+//   console.log('Got it', data);
+// });
+
+jQuery('#message-form').on('submit', function(e) {
+  e.preventDefault();
+
+  socket.emit('createMessage', {
+    from: 'User',
+    text: jQuery('[name=message]').val()
+  }, function () {
+
+  });
 });
