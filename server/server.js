8050ab4bf818b63539058ebe6f8b44596334463f
Tham Treerawuttichaikit <intime_@outlook.com>
2017-11-02 10:21:22 +0700

    add newMessage and createMessage Event

--- server/server.js
+++ server/server.js
@@ -16,6 +16,16 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
+  socket.emit('newMessage', {
+    from: 'a@example.com',
+    text: 'Hello client user',
+    createdAt: 214
+  });
+
+  socket.on('createMessage', (message) => {
+    console.log('createMessage', message);
+  })
+
   socket.on('disconnect', () => {
     console.log('User was disconnected');
   });
